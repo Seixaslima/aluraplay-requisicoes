be9afd740864adbcb961fb723b0a8daf9e2fc6f7
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-06-02 14:42:34 -0300

    unidade 4 finalizada

---
+++ js/buscarVideo.js
@@ -0,0 +1,29 @@
+import { conectaApi } from './conectaApi.js';
+import constroiCard from './mostrarVideos.js';
+
+async function buscarVideo(evento) {
+  evento.preventDefault();
+  const dadosDePesquisa = document.querySelector('[data-pesquisa]').value;
+
+  const videos = await conectaApi.buscaVideos(dadosDePesquisa);
+
+  const lista = document.querySelector('[data-lista]');
+
+  while (lista.firstChild) {
+    lista.removeChild(lista.firstChild);
+  }
+
+  videos.forEach(element => {
+    lista.appendChild(
+      constroiCard(
+        element.titulo,
+        element.descricao,
+        element.url,
+        element.image
+      )
+    );
+  });
+}
+
+const botaoDePesquisa = document.querySelector('[data-botao-pesquisa]');
+botaoDePesquisa.addEventListener('click', e => buscarVideo(e));
